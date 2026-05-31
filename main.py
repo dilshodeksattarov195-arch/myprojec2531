df254e014d6746568d58e6114042eb2ae737ac90
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 12:06:40 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class sessionFtringifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = sessionFtringifyWorker()
+    print("Worker engine initialized.")
